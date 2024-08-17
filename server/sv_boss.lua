1876aca28ddd3cd786bf244f4b7b8fb96ed3aeec
SluggDev <100637730+Lalimace420@users.noreply.github.com>
2024-08-17 18:39:33 -0400

    sv_boss.lua

--- server/sv_boss.lua
+++ server/sv_boss.lua
@@ -1,4 +1,5 @@
 local QBCore = exports['qb-core']:GetCoreObject()
+local Accounts = {}
 
 function ExploitBan(id, reason)
 	MySQL.insert('INSERT INTO bans (name, license, discord, ip, reason, expire, bannedby) VALUES (?, ?, ?, ?, ?, ?, ?)', {
@@ -13,6 +14,94 @@ function ExploitBan(id, reason)
 	TriggerEvent('qb-log:server:CreateLog', 'bans', 'Player Banned', 'red', string.format('%s was banned by %s for %s', GetPlayerName(id), 'qb-management', reason), true)
 	DropPlayer(id, 'You were permanently banned by the server for: Exploiting')
 end
+
+
+
+function GetAccount(account)
+	return Accounts[account] or 0
+end
+
+function AddMoney(account, amount)
+	if not Accounts[account] then
+		Accounts[account] = 0
+	end
+
+	Accounts[account] = Accounts[account] + amount
+	MySQL.insert('INSERT INTO management_funds (job_name, amount, type) VALUES (:job_name, :amount, :type) ON DUPLICATE KEY UPDATE amount = :amount',
+		{
+			['job_name'] = account,
+			['amount'] = Accounts[account],
+			['type'] = 'boss'
+		})
+end
+
+function RemoveMoney(account, amount)
+	local isRemoved = false
+	if amount > 0 then
+		if not Accounts[account] then
+			Accounts[account] = 0
+		end
+
+		if Accounts[account] >= amount then
+			Accounts[account] = Accounts[account] - amount
+			isRemoved = true
+		end
+
+		MySQL.update('UPDATE management_funds SET amount = ? WHERE job_name = ? and type = "boss"', { Accounts[account], account })
+	end
+	return isRemoved
+end
+
+MySQL.ready(function ()
+	local bossmenu = MySQL.query.await('SELECT job_name,amount FROM management_funds WHERE type = "boss"', {})
+	if not bossmenu then return end
+
+	for _,v in ipairs(bossmenu) do
+		Accounts[v.job_name] = v.amount
+	end
+end)
+
+
+RegisterNetEvent("qb-bossmenu:server:withdrawMoney", function(amount)
+	local src = source
+	local Player = QBCore.Functions.GetPlayer(src)
+
+	if not Player.PlayerData.job.isboss then ExploitBan(src, 'withdrawMoney Exploiting') return end
+
+	local job = Player.PlayerData.job.name
+	if RemoveMoney(job, amount) then
+		Player.Functions.AddMoney("cash", amount, 'Boss menu withdraw')
+		TriggerEvent('qb-log:server:CreateLog', 'bossmenu', 'Withdraw Money', "blue", Player.PlayerData.name.. "Withdrawal $" .. amount .. ' (' .. job .. ')', false)
+		TriggerClientEvent('QBCore:Notify', src, "You have withdrawn: $" ..amount, "success")
+	else
+		TriggerClientEvent('QBCore:Notify', src, "You dont have enough money in the account!", "error")
+	end
+
+	TriggerClientEvent('qb-bossmenu:client:OpenMenu', src)
+end)
+
+RegisterNetEvent("qb-bossmenu:server:depositMoney", function(amount)
+	local src = source
+	local Player = QBCore.Functions.GetPlayer(src)
+
+	if not Player.PlayerData.job.isboss then ExploitBan(src, 'depositMoney Exploiting') return end
+
+	if Player.Functions.RemoveMoney("cash", amount) then
+		local job = Player.PlayerData.job.name
+		AddMoney(job, amount)
+		TriggerEvent('qb-log:server:CreateLog', 'bossmenu', 'Deposit Money', "blue", Player.PlayerData.name.. "Deposit $" .. amount .. ' (' .. job .. ')', false)
+		TriggerClientEvent('QBCore:Notify', src, "You have deposited: $" ..amount, "success")
+	else
+		TriggerClientEvent('QBCore:Notify', src, "You dont have enough money to add!", "error")
+	end
+
+	TriggerClientEvent('qb-bossmenu:client:OpenMenu', src)
+end)
+
+QBCore.Functions.CreateCallback('qb-bossmenu:server:GetAccount', function(_, cb, jobname)
+	local result = GetAccount(jobname)
+	cb(result)
+end)
 
 -- Get Employees
 QBCore.Functions.CreateCallback('qb-bossmenu:server:GetEmployees', function(source, cb, jobname)
